bfdad4cd8b2bef450ec65855af2f20e17156947a
Kevin Rodrigues <kevin.rodrigues@just-eat.com>
2020-01-29 20:30:08 +0000

    PS-1 - Add vue logging option.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueScrollTo from 'vue-scrollto';
 import * as Sentry from '@sentry/browser';
 import LogRocket from 'logrocket';
+import * as Integrations from '@sentry/integrations';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -9,7 +10,11 @@ import store from './store';
 Vue.use(VueScrollTo);
 Vue.config.productionTip = false;
 
-Sentry.init({ dsn: 'https://d5d2bf2ee9f049e0bb03f4ecd1665b13@sentry.io/2067207' });
+Sentry.init({
+  dsn: 'https://d5d2bf2ee9f049e0bb03f4ecd1665b13@sentry.io/2067207',
+  integrations: [new Integrations.Vue({Vue, attachProps: true})],
+});
+
 LogRocket.init('stmphc/personal-site-v6go3');
 
 new Vue({
