0ad497157e3823e473ce6042cbf2722220a7dcc1
Kevin Rodrigues <kevin.rodrigues@just-eat.com>
2020-02-07 16:18:28 +0000

    PS-1 - Test form integration with netlify.

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -15,18 +15,15 @@
     >
       <input type="hidden" name="form-name" value="contactus" />
       <div>
-        <label for="name">Name:</label>
         <input type="text" name="name" required/>
       </div>
       <div>
         <input type="email" name="email" required/>
-        <label for="email">Email:</label>
       </div>
       <div>
         <textarea name="message" required></textarea>
-        <label for="message">Message:</label>
       </div>
-      <button type="submit" value="Send message">Send</button>
+      <button type="submit" class="btn" value="Send message">Send</button>
     </form>
   </div>
 </template>
